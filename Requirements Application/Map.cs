ecc5982bea73604de657e952f4141537a5769907
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-11 01:03:04 -0600

    First Commit

---
+++ Requirements Application/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirements_Application
+{
+    abstract public class Map
+    {
+        public int RequirementNumber { get; set; }
+
+        public string Tool { get; set; }
+
+        public bool FullyFulfills { get; set; }
+    }
+}
