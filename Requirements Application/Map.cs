1389269a3929ade170803f47c95cb89348134575
ethansudman <ethansudman@comcast.net>
2014-11-11 20:24:54 -0600

    Commit adds

---
+++ Requirements Application/Map.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirements_Application
+{
+    abstract public class Map
+    {
+        public int RequirementNumber { get; set; }
+
+        public string Tool { get; set; }
+
+        public bool FullyFulfills { get; set; }
+
+        public abstract string Description { get; }
+
+        public abstract string ToolName { get; }
+
+        /// <summary>
+        /// Factory to get a <see cref="Connection"/> to the underlying provider
+        /// </summary>
+        /// <returns></returns>
+        abstract protected Connection ConnectToTool();
+    }
+}
---
+++ Requirements Application/Providers/AccessProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Requirements_Application
+{
+    public class AccessProvider : Map
+    {
+        protected override Connection ConnectToTool()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToolName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string Description
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
