e7dfc0c8c517114ea9fc622bfa2d5585959ad8e6
ethansudman@comcast.net <ethansudman@comcast.net>
2014-11-12 01:10:21 -0600

    Add unit tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Requirements_Application;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExcelTestMethod1()
+        {
+            ExcelProvider provider = new ExcelProvider();
+            provider.RequirementNumber = 2;
+            provider.Init();
+
+            Assert.AreEqual("Yes", provider.Description);
+
+            Assert.AreEqual("Some Other Req", provider.Name);
+        }
+    }
+}
